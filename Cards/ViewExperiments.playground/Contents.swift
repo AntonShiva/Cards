dde1727c2b01e131e7e24fa3b57d4ec131e15d21
AntonShiva <ryashinanthon@yandex.ru>
2023-08-28 08:47:32 +0300

    добавил еще пару вью

--- Cards/ViewExperiments.playground/Contents.swift
+++ Cards/ViewExperiments.playground/Contents.swift
@@ -14,10 +14,18 @@ import PlaygroundSupport
             self.view = getRootView()
             let greenView = getGreenView()
             let blyView = getBlyView()
+            let yellowView = getYellowView()
+            // фиолетовый вью
+            let filletView = getFilletView()
+            
             setCentr(view: blyView, toCenterOfView: greenView)
+            // выравнить фиолетове вью по центру
+            filletView.center = blyView.center
             
             self.view.addSubview( greenView )
+            self.view.addSubview(yellowView)
             greenView.addSubview( blyView )
+            greenView.addSubview(filletView)
         }
         // создание корневого view
         private func getRootView() -> UIView {
@@ -25,20 +33,35 @@ import PlaygroundSupport
             view.backgroundColor =  #colorLiteral(red: 0, green: 1, blue: 1, alpha: 1)
             return view
         }
-        // создание квадрата в нутри корневого view
+        // создание зеленoго квадрата в нутри корневого view
         private func getGreenView() ->  UIView {
-            let viewFrame = CGRect(x: 50, y: 50, width: 250, height: 220)
+            let viewFrame = CGRect(x: 50, y: 50, width: 200, height: 200)
             let view = UIView(frame: viewFrame)
             view.backgroundColor = #colorLiteral(red: 0, green: 0.9597107768, blue: 0.2508707047, alpha: 1)
             view.clipsToBounds = true
             return view
         }
+        // создание голубого квадрата
         private func getBlyView() -> UIView {
             let viewFrame = CGRect(x: 10, y: 10, width: 180, height: 180)
             let view = UIView(frame: viewFrame)
             view.backgroundColor = #colorLiteral(red: 0, green: 0.3623378277, blue: 1, alpha: 1)
             return view
             
+        }
+        // создание фиолетового квадрата
+        private func getFilletView() -> UIView {
+            let viewFrame = CGRect(x: 0, y: 0, width: 50, height: 50)
+            let view = UIView(frame: viewFrame)
+            view.backgroundColor = #colorLiteral(red: 0.8852633834, green: 0.4954180717, blue: 1, alpha: 1)
+            return view
+        }
+        // создание желтого квадрата
+        private func getYellowView() -> UIView {
+            let viewFrame = CGRect(x: 50, y: 300, width: 100, height: 100)
+            let view = UIView(frame: viewFrame)
+            view.backgroundColor = #colorLiteral(red: 0.975792706, green: 1, blue: 0.2305567265, alpha: 1)
+            return view
         }
         private func setCentr(view moveView: UIView,toCenterOfView baseView: UIView) {
             // размеры вложенного предстовления

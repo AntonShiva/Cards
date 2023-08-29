a9d490161f51b720dfa93ecf75a007747261a160
AntonShiva <ryashinanthon@yandex.ru>
2023-08-29 16:54:22 +0300

    поморская шапочка кривыми Безье

--- Cards/BezierExperiments.playground/Contents.swift
+++ Cards/BezierExperiments.playground/Contents.swift
@@ -53,9 +53,27 @@ class MyViewController : UIViewController {
 //        path.addLine(to: CGPoint(x: 50, y: 170))
 //        path.close()
         
-        // создание сущности "Прямоугольник"
-        let rect = CGRect(x: 10, y: 10, width: 200, height: 100) // создание прямоугольника
-        let path = UIBezierPath(roundedRect: rect, cornerRadius: 20)
+//        // создание сущности "Прямоугольник"
+//        let rect = CGRect(x: 10, y: 10, width: 200, height: 100)
+//        let path = UIBezierPath(roundedRect: rect,
+//                                byRoundingCorners: [.bottomRight, .topLeft, .topRight], cornerRadii: CGSize(width: 10, height: 0))
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 100, y: 100))
+        path.addArc(withCenter: CGPoint(x: 150, y: 100), radius: 50,
+        startAngle: .pi, endAngle: 0, clockwise: true)
+        path.addLine(to: CGPoint(x: 220, y: 100))
+        path.addArc(withCenter: CGPoint(x: 220, y: 150), radius: 50,
+        startAngle: .pi*3/2, endAngle: .pi/2, clockwise: true)
+        path.addLine(to: CGPoint(x: 200, y: 200))
+        path.addLine(to: CGPoint(x: 200, y: 260))
+        path.addLine(to: CGPoint(x: 100, y: 260))
+        path.addLine(to: CGPoint(x: 100, y: 200))
+        path.addLine(to: CGPoint(x: 80, y: 200))
+        path.addArc(withCenter: CGPoint(x: 80, y: 150),
+        radius: 50,
+        startAngle: .pi/2, endAngle: .pi*3/2, clockwise: true)
+        path.close()
         return path
         
     }

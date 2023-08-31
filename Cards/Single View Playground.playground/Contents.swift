4aa4d5bdc8f26f48ac91eb9b3e724b833eb7382e
AntonShiva <ryashinanthon@yandex.ru>
2023-08-31 12:36:23 +0300

    добавил два класса отвечающих за фон круги

--- Cards/Single View Playground.playground/Contents.swift
+++ Cards/Single View Playground.playground/Contents.swift
@@ -96,3 +96,45 @@ required init?(coder: NSCoder) { fatalError("init(coder:) has not been implement
     
 }
 
+class FillShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+    super.init()
+    let path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+    self.path = path.cgPath
+    self.fillColor = fillColor
+    }
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented")
+   }
+}
+class BackSideCircle: CAShapeLayer, ShapeLayerProtocol {
+    
+    required init(size: CGSize, fillColor: CGColor) {
+    super.init()
+    
+    let path = UIBezierPath()
+    
+    // рисуем 15 кругов
+    for _ in 1...15 {
+    // координаты центра очередного круга
+    let randomX = Int.random(in: 0...Int(size.width))
+    let randomY = Int.random(in: 0...Int(size.height))
+    let center = CGPoint(x: randomX, y: randomY)
+    // смещаем указатель к центру круга
+    path.move(to: center)
+    // определяем случайный радиус
+    let radius = Int.random(in: 5...15)
+    // рисуем круг
+    path.addArc(withCenter: center, radius: CGFloat(radius),startAngle: 0, endAngle: .pi*2, clockwise: true)
+ }
+    // инициализируем созданный путь
+    self.path = path.cgPath
+    // изменяем цвет
+    self.strokeColor = fillColor
+    self.fillColor = fillColor
+    self.lineWidth = 1
+   }
+   required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented")
+}
+
+}
+
